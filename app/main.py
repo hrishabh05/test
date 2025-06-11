94d9b1b5a09717aa054f123f2235e5faefd5a3c4
hrishabh05 <121535715+hrishabh05@users.noreply.github.com>
2025-06-11 16:03:43 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "✅ Hello from your GitHub Actions demo app!"
+    return "✅ Hello from your GitHub Actions demo app! vishwash"
 
 @app.route("/healthz")
 def health():
